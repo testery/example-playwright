5a490db6b0b50aa26a737a49fdbed7d05277745e
Justin Heffernan <justinth9543@gmail.com>
2023-08-01 16:23:33 -0500

    Added a test that should fail

--- tests/another-test-folder/example-three.spec.js
+++ tests/another-test-folder/example-three.spec.js
@@ -17,3 +17,13 @@ test('get started link three @getStartedLink', async ({ page }) => {
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
 });
+
+test('get started link three that fails @getStartedLink @fail', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*fail/);
+});
