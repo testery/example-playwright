518d4ab3c3dc3e17ed26842a74348cbebe2fb5fb
Adam England <adam.n.england@gmail.com>
2024-04-01 13:20:48 -0500

    Flaky Test Example

---
+++ tests/quarantine.spec.js
@@ -0,0 +1,10 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('flaky test', async ({ page }) => {
+  Math.random() > 0.5 ? expect(1).toBe(1) : expect(1).toBe(2);
+});
+
+test('solid test', async ({ page }) => {
+  expect(1).toBe(1);
+});
