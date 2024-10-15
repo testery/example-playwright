21d21107d550918b6aae4a2a35a510db9d5e7499
Adam England <adam.n.england@gmail.com>
2024-10-15 15:56:16 -0500

    more

--- tests/global.setup.js
+++ tests/global.setup.js
@@ -2,6 +2,6 @@ import { test as setup } from '@playwright/test';
 
 setup('create new database', async ({ }) => {
     if (Math.random() > 0.5) {
-        throw new Error('Random failure');
+        console.log("yeah dude" + x)
       }
 });
