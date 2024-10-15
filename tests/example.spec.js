69e44d92d94ebacf2be2b5af436336f8485f8894
Adam England <adam.n.england@gmail.com>
2024-10-15 10:59:01 -0500

    Replicate production issue

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,6 +4,9 @@ const { test, expect } = require('@playwright/test');
 test('has title one @titleTest', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
+  if (Math.random() > 0.5) {
+    throw new Error('Random failure');
+  }
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
@@ -14,6 +17,11 @@ test('get started link one @getStartedLink', async ({ page }) => {
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
+  
+  if (Math.random() > 0.5) {
+    throw new Error('Random failure');
+  }
+  
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
 });
