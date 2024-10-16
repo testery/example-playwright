399c9f2fa6c22f41ecd40d51ef7b8643a8b5ec81
Will Gunn <will.gunn@testery.io>
2024-10-16 12:31:42 -0500

    example

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,13 +7,3 @@ test('has title one @titleTest', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
-
-test('get started link one @getStartedLink', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
