9cfc5a535e4a1e542c4eefe59d4a02de31168702
Will Gunn <will.gunn@testery.io>
2024-10-23 08:33:48 -0500

    try tag option

--- tests/example-two.spec.js
+++ tests/example-two.spec.js
@@ -1,16 +1,17 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test.describe("group 1 @test", () => {
+test.describe('two tagged tests', {
+  tag: '@smoke',
+}, () => {
   test.beforeEach( async ({ page }) => { 
     await page.goto('https://playwright.dev/');
   })
-
-  test('has title two (with screenshot) @titleTest', async ({ page }) => {
-  
-    await page.screenshot({path: "./screenshots/has_title_two.png"});
-  
-    // Expect a title "to contain" a substring.
+  test('one', async ({ page }) => {
     await expect(page).toHaveTitle(/Playwright/);
   });
-})
+
+  test('two', async ({ page }) => {
+    await expect(page).toHaveTitle(/Playwright/);
+  });
+});
